333010bd7bfb3feb82d4ed46a67049ce36ccbaa0
Jeff Huang <jeffshuang2@gmail.com>
2019-11-13 10:37:46 -0800

    Successfully created a connection with the mySQL database using localhost.

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var db = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "schumacher1",
+    database: "bamazon_db"
+});
+
+db.connect(function(err) {
+    if (err) throw (err);
+    // runBamazon();
+    console.log("Connection successful.");
+});
+
