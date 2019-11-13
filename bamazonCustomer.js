a625e3d94d684912c444c6cf41e40c8560ed6898
Jeff Huang <jeffshuang2@gmail.com>
2019-11-13 13:45:04 -0800

    Created inquirer questions and variable to store the new available quantity of the item after purchase.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,7 +14,71 @@ var db = mysql.createConnection({
 
 db.connect(function(err) {
     if (err) throw (err);
-    // runBamazon();
     console.log("Connection successful.");
+    runBamazon();
 });
 
+function runBamazon () {
+    var query = "SELECT item_id, product_name, price FROM products";
+    db.query(query, function(err, res) {
+        console.log("==================================" + "\n");
+        console.log("ITEMS FOR SALE");
+        for (var i = 0; i < res.length; i++) {
+            console.log("----------------------------------");
+            console.log(
+                "Item ID: " + res[i].item_id + "\nProduct name: " + res[i].product_name + "\nPrice: $" + res[i].price
+            );
+        }
+        console.log("\n" + "==================================");
+        runInquirer();
+    });
+};
+    
+function runInquirer() {
+    db.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+    
+    inquirer
+        .prompt([{
+            name: "itemID",
+            type: "input",
+            message: "What's the ID of the item you would like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+        },
+        {
+            name: "howManyUnits",
+            type: "input",
+            message: "How many units of the item would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        ])
+        .then(function(answer) {
+            var newItemQuantity;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === parseFloat(answer.itemID) && res[i].stock_quantity >= parseFloat(answer.howManyUnits)) {
+                   newItemQuantity = res[i].stock_quantity - answer.howManyUnits;
+                }
+            }
+            console.log(newItemQuantity);
+        //     if (newItemQuantity >= parseFloat(answer.itemID) {
+        //         newItemQuantity 
+        //     })
+        // })
+        })
+    })
+};
+
+
+// function updateBamazon() {
+//     var query = "UPDATE products SET stock_quantity = ?"
+// }
